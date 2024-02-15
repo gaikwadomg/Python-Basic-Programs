e7b80051579c461d8f68a3896983ea63b0f66a8f
gaikwadomg <connect2ompc@gmail.com>
2024-02-15 11:42:33 +0530

    factorial

---
+++ learn.py
@@ -0,0 +1,16 @@
+# Write programs to solve simple problems, such as calculating the factorial of a number, finding prime numbers, or generating Fibonacci sequences.
+
+# factorial of a number 5! is 5 * 4 * 3 * 2 * 1  = 5!  ;   
+import math
+x = input("enter factorial value : ")
+x = int(x)
+print(math.factorial(x))
+
+# num = input("enter your factorial number : ")
+# num = int(num);
+# for i in 5:
+#     fact = num * i ;
+#     num = num-1;
+    
+# print(fact)
+    
